c91237e4591a5a1f9a47afefb21c9fe1c68e0ffb
contra <yo@contra.io>
2018-07-13 19:45:59 -0400

    syntax

--- update/aof.js
+++ update/aof.js
@@ -3,17 +3,17 @@ import through2 from 'through2'
 export default () =>
   through2.obj((cmd, _, cb) => {
     let arg
-    const op = cmd.shift();
+    const op = cmd.shift()
     const aof = [
       '*' + (cmd.length + 1),
       '$' + op.length,
       op
-    ];
+    ]
     while (cmd.length) {
-      arg = cmd.shift();
-      aof.push('$' + arg.length);
-      aof.push(arg);
+      arg = cmd.shift()
+      aof.push('$' + arg.length)
+      aof.push(arg)
     }
 
-    cb(null, aof.join('\r\n') + '\r');
+    cb(null, aof.join('\r\n') + '\r')
   })
